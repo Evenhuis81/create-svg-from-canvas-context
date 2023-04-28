9755b78bf8db10eef9bc48432f214deb47477ce9
EvenhuisD <devenhuis81@gmail.com>
2023-04-28 14:38:02 +0200

    main page canvas and svg show + example svg

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,59 @@
 <template>
-    <div
-        style="
-            width: 100vw;
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        "
-    >
-        <canvas
-            id="canvas1"
-            style="border: 1px solid black"
-            width="320"
-            height="180"
-        />
+    <div id="container">
+        <div
+            style="
+                display: flex;
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 24px;
+            "
+        >
+            <div style="width: 320px; text-align: center">Canvas:</div>
+            <div style="width: 320px; text-align: center; margin-left: 10px">
+                SVG:
+            </div>
+        </div>
+        <div id="svgAppend" style="display: flex">
+            <canvas
+                id="canvas1"
+                style="border: 1px solid black"
+                width="320"
+                height="180"
+            />
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue';
-// import { getCanvas, getContext2D } from './canvasAPI';
 import { createSVG } from './canvasAPI/canvas2svg';
-
-const eventTemplateSvg = (c: CanvasRenderingContext2D) => {
-    c.strokeStyle = 'rgb(100, 100, 100)';
-    c.lineWidth = 16;
-    c.lineCap = 'round';
-
-    c.beginPath();
-    c.moveTo(100, 39.5);
-    c.lineTo(220, 39.5);
-    c.stroke();
-
-    c.lineWidth = 2;
-
-    c.beginPath();
-    c.strokeRect(80, 75, 160, 75);
-
-    c.fillStyle = 'rgb(100, 100, 100)';
-
-    c.beginPath();
-    c.font = 'bold 32px arial';
-    c.textAlign = 'center';
-    c.textBaseline = 'middle';
-    c.fillText('EVENTS', 160, 115);
-};
+import { eventTemplateSvg } from './templates/events';
+import { getCanvas, getContext2D } from './canvasAPI';
 
 onMounted(() => {
-    // const canvas = getCanvas('canvas1');
-    // const context = getContext2D(canvas);
+    const canvas = getCanvas('canvas1');
+    const context = getContext2D(canvas);
 
-    // templateSvg(context);
-    createSVG(320, 180, eventTemplateSvg);
+    eventTemplateSvg(context);
+
+    const svg = createSVG(320, 180, eventTemplateSvg);
+
+    svg.style.marginLeft = '10px';
+    svg.style.border = '1px solid black';
+
+    const svgAppend = document.getElementById('svgAppend');
+
+    svgAppend?.appendChild(svg);
 });
 </script>
+
+<style scoped>
+#container {
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+</style>
